decc4abd0f0d6f7b912c91fa5c4e271b276a78ae
Shivam Gargsya <shivamgargsya@Shivams-MacBook-Pro.local>
2019-11-19 15:45:41 +0530

    * removed unnecessary files

--- flask_app.py
+++ flask_app.py
@@ -8,13 +8,6 @@ from flask import request
 from flask_caching import Cache
 from flask_cors import CORS
 
-from dotenv import load_dotenv
-import os
-from pathlib import Path  # python3 only
-dir_path=os.path.dirname(os.path.abspath(__file__))
-env_path = dir_path+  '/.env'
-load_dotenv(dotenv_path=env_path)
-
 
 from Config import config
 import time
@@ -56,42 +49,4 @@ logging.basicConfig(format='[%(asctime)s] %(levelname)s:%(message)s')
 def get_flask_app():
     return flask_app
 
-from hora.notification.queue.Fifo_queue import Fifo_queue
-from hora.notification.queue.Queue_registry import Queue_registry
-from hora.notification.dispatcher.dispatcher import Dispatcher
-
-def setup():
-    queue=Fifo_queue('consumer_action_queue')
-    Queue_registry.getInstance().register('consumer_action_queue',queue)
-
-def dispatcher_job():
-    while True:
-        try:
-            queues=Queue_registry.getInstance().get_queues()
-            for queue in queues:
-                Dispatcher(list(queue.values())[0]).dispatch_items()
-                time.sleep(10)
-        except Exception as e:
-            print(str(e))
-
-
-
-from hora.consumer.resources.consumer_resources import *
-from hora.worker.resources.worker_resource import *
-from hora.tasks.resources.task_resource import *
-from hora.category.resources.category_resource import *
-from hora.commons.resources.authorisation.authorisation_resource import *
-from hora.commons.resources.health_check import *
-from hora.ratings.resources.ratings_resource import *
-
-
-setup()
-
-from time import sleep
-from concurrent.futures import ThreadPoolExecutor
-
-# DOCS https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor
-executor = ThreadPoolExecutor(1)
-poll_executor=ThreadPoolExecutor(1)
-poll_executor.submit(dispatcher_job)
 
--- v1_api.py
+++ v1_api.py
@@ -9,10 +9,6 @@ api = Api(apiv1, version='1.0', title='RPA v1 APIs', description='v1 apis')
 
 healthcheck_api = api.namespace('Health Check APIs', description='Health Check API methods', path='/')
 
-auth_api=api.namespace('Authorisation',description='',path='/')
-task_api=api.namespace('Task',description='API for CRU operations and get tasks related to worker consumer ',path='/')
-worker_api=api.namespace('Worker',description='API for CRU operations on workers ',path='/')
-consumer_api=api.namespace('Consumer',description='API for CRU operations on consumers ',path='/')
-category_api=api.namespace('Category',description='API for CRU operations on categories ',path='/')
-rating_api=api.namespace('Rating',description='API for CRU operations on ratings ',path='/')
+
+rating_api=api.namespace('Recepie',description='API for Recepie adding operations ',path='/')
 flask_app.register_blueprint(apiv1)
